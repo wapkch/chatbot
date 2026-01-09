2788b08c7431eb399fa290c5de53296f561e7e37
za-wangchao003 <wangchao003@zhongan.io>
2026-01-09 10:39:55 +0800

    Initial Commit

---
+++ chatme/chatmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatmeApp.swift
+//  chatme
+//
+//  Created by wangchao on 2026/1/9.
+//
+
+import SwiftUI
+
+@main
+struct chatmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
